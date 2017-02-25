52dcb27a0643959bdf93f70e0feec855fd047081
Sabeur Thabti <thabti.sabeur@gmail.com>
2017-02-25 20:16:56 +0000

    quran.com eslint rules and fixes

--- src/container/Home.js
+++ src/container/Home.js
@@ -2,16 +2,14 @@ import React from 'react';
 import Button from '../../components/Button';
 import Package from '../../package.json';
 import styles from './style.scss';
-const Home = () => {
-	return (
-		<div className={styles.app}>
-			<h1>Quran Common Components {Package.version}</h1>
-			<hr />
-			<section>
-				<Button>Button Example</Button>
-			</section>
-		</div>
-	);
-};
+
+const Home = () => (
+  <div className={styles.app}>
+    <h1>Quran Common Components {Package.version}</h1>
+    <hr />
+    <section>
+      <Button>Button Example</Button>
+    </section>
+  </div>);
 
 export default Home;
--- src/main.js
+++ src/main.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
 import Root from './Root';
+
 const render = (Component) => {
   ReactDOM.render(
     <AppContainer>
       <Component />
-    </AppContainer>,
-    document.getElementById('root')
+    </AppContainer>, document.getElementById('root')
   );
 };
 
