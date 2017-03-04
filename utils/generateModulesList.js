9fb87dc4e18eda5d4c22ffad6b33f3e418abf59c
Sabeur Thabti <thabti.sabeur@gmail.com>
2017-03-04 21:26:39 +0000

    [ci skip] add ci skip to git pages

--- utils/generateModulesList.js
+++ utils/generateModulesList.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 fs.readdir('./lib', (err, files) => {// eslint-disable-line
-  if (err) return console.log(err);
+  if (err) return console.log(err);// eslint-disable-line
   const fileTemplate = filesList => filesList.map(file => `exports.${file} = require('./${file}').default;`).join('\n');
 	fs.writeFile('./lib/index.js', fileTemplate(files), error => error ? console.log(error) : console.log('The file was saved!'));// eslint-disable-line
 });
