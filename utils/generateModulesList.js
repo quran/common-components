2395a1906dce9495846b9996661b3b8aac4a5eda
Sabeur Thabti <thabti.sabeur@gmail.com>
2017-03-05 00:54:15 +0000

    exclude styles and images from the generated index.js

--- utils/generateModulesList.js
+++ utils/generateModulesList.js
@@ -1,7 +1,11 @@
+/*eslint-disable */
 const fs = require('fs');
 
-fs.readdir('./lib', (err, files) => {// eslint-disable-line
-  if (err) return console.log(err);// eslint-disable-line
-  const fileTemplate = filesList => filesList.map(file => `exports.${file} = require('./${file}').default;`).join('\n');
-  fs.writeFile('./lib/index.js', fileTemplate(files), error => error ? console.log(error) : console.log('The file was saved!'));// eslint-disable-line
+fs.readdir('./lib', (err, files) => {
+  if (err) return console.log(err);
+  const filteredFolder = files.filter(item => new RegExp(/^(?!.*images|styles).*$/g).test(item));
+  const fileTemplate = folders => folders.map(folder => `exports.${folder} = require('./${folder}').default;`).join('\n');
+  fs.writeFile('./lib/index.js', fileTemplate(filteredFolder), error => error ? console.log(error) : console.log(`The file was saved! Components: ${filteredFolder.join(', ')}`));
 });
+
+/*eslint-enable */
