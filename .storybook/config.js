8605eebf2fd7b3f7083c90656bd4f0ce5aa97e96
Mohamed El Mahallawy <mmahalwy@gmail.com>
2017-03-06 00:01:34 -0800

    Add info for storybook (#10)

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,6 +1,10 @@
-import { configure } from '@kadira/storybook';
+import { configure, setAddon } from '@kadira/storybook';
 import { setOptions } from '@kadira/storybook-addon-options';
+import infoAddon from '@kadira/react-storybook-addon-info';
+
 const req = require.context('../src', true, /.stories.js$/)
+
+setAddon(infoAddon);
 
 function loadStories() {
   req.keys().forEach((filename) => req(filename))
--- src/Button/index.stories.js
+++ src/Button/index.stories.js
@@ -3,8 +3,8 @@ import { storiesOf, action } from '@kadira/storybook';
 import Button from './index';
 
 storiesOf('Button', module)
-  .add('with text', () => (
-    <Button onClick={action('clicked')}>Hello Button!</Button>
+  .addWithInfo('simple usage', 'Basic usage of the button', () => (
+    <Button onClick={action('clicked')}>Hello Button</Button>
   ))
   .add('with some emoji', () => (
     <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>
