8141fbd39e19dda58dba0f0c91860bb7904e4293
Sabeur Thabti <thabti.sabeur@gmail.com>
2017-03-10 08:43:10 +0000

    update button story

--- src/Button/index.stories.js
+++ src/Button/index.stories.js
@@ -15,12 +15,6 @@ storiesOf('Button', module)
   .add('Inverted', () => (
     <Button color="inverted">Inverted</Button>
   ))
-  .add('Link (Provide href)', () => (
+  .add('With href', () => (
     <Button href="http://Quran.com" target="_blank">😎 👍 💯 Quran.com</Button>
-  ))
-  .add('Link disabled', () => (
-    <Button href="http://Quran.com" target="_blank" disabled>😎 👍 💯 Quran.com</Button>
-  ))
-  .add('Link Inverted', () => (
-    <Button color="inverted" href="http://Quran.com" target="_blank">😎 👍 💯 Quran.com</Button>
   ));
