3e6d428d977a2d2251b414de6568d3163e5c0184
Sabeur Thabti <thabti.sabeur@gmail.com>
2017-03-12 08:42:59 +0000

    [ci skip] add unit tests

--- src/Button/test/button.test.js
+++ src/Button/test/button.test.js
@@ -5,7 +5,7 @@ import Button from '../index';
 describe('<Button />', () => {
   it('Should Render and simulate click ', () => {
     const onClickSpy = sinon.spy();
-    const component = shallow(<Button onClick={() => onClickSpy()} />);
+    const component = shallow(<Button onClick={() => onClickSpy()}>Hello</Button>);
     expect(component.find('button').exists()).to.eql(true);
     component.find('button').simulate('click');
     expect(onClickSpy).to.have.called; // eslint-disable-line
