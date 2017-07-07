65dc5bf81480851d25a6bfee23e6c5deed08da56
Mohamed El Mahallawy <mmahalwy@gmail.com>
2017-07-07 01:49:30 -0700

    Moving to styled-components

---
+++ src/Icon/types.js
@@ -0,0 +1,372 @@
+export default {
+  cursor: '',
+
+  crosshair: '⌖',
+
+  search: '🔎',
+
+  zoomin: '',
+
+  zoomout: '',
+
+  view: '👀',
+
+  attach: '📎',
+
+  link: '🔗',
+
+  move: '',
+
+  write: '✎',
+
+  writingdisabled: '',
+
+  erase: '✐',
+
+  compose: '📝',
+
+  lock: '🔒',
+
+  unlock: '🔓',
+
+  key: '🔑',
+
+  backspace: '⌫',
+
+  ban: '🚫',
+
+  trash: '',
+
+  target: '◎',
+
+  tag: '',
+
+  bookmark: '🔖',
+
+  flag: '⚑',
+
+  like: '👍',
+
+  dislike: '👎',
+
+  heart: '♥',
+
+  halfheart: '',
+
+  star: '⋆',
+
+  halfstar: '',
+
+  sample: '',
+
+  crop: '',
+
+  layers: '',
+
+  fill: '',
+
+  stroke: '',
+
+  phone: '📞',
+
+  phonedisabled: '',
+
+  rss: '',
+
+  facetime: '',
+
+  reply: '↩',
+
+  send: '',
+
+  mail: '✉',
+
+  inbox: '📥',
+
+  chat: '💬',
+
+  ellipsischat: '',
+
+  ellipsis: '…',
+
+  user: '👤',
+
+  femaleuser: '👧',
+
+  users: '👥',
+
+  cart: '',
+
+  creditcard: '💳',
+
+  dollarsign: '💲',
+
+  barchart: '📊',
+
+  piechart: '',
+
+  box: '📦',
+
+  home: '⌂',
+
+  buildings: '🏢',
+
+  warehouse: '',
+
+  globe: '🌎',
+
+  navigate: '',
+
+  compass: '',
+
+  signpost: '',
+
+  map: '',
+
+  location: '',
+
+  pin: '📍',
+
+  database: '',
+
+  hdd: '',
+
+  music: '♫',
+
+  mic: '🎤',
+
+  volume: '🔈',
+
+  lowvolume: '🔉',
+
+  highvolume: '🔊',
+
+  airplay: '',
+
+  camera: '📷',
+
+  picture: '🌄',
+
+  video: '📹',
+
+  play: '▶',
+
+  pause: '',
+
+  stop: '■',
+
+  record: '●',
+
+  rewind: '⏪',
+
+  fastforward: '⏩',
+
+  skipback: '⏮',
+
+  skipforward: '⏭',
+
+  eject: '⏏',
+
+  repeat: '🔁',
+
+  replay: '↺',
+
+  shuffle: '🔀',
+
+  book: '📕',
+
+  openbook: '📖',
+
+  notebook: '📓',
+
+  newspaper: '📰',
+
+  grid: '',
+
+  rows: '',
+
+  columns: '',
+
+  thumbnails: '',
+
+  filter: '',
+
+  desktop: '💻',
+
+  laptop: '',
+
+  tablet: '',
+
+  cell: '📱',
+
+  battery: '🔋',
+
+  highbattery: '',
+
+  mediumbattery: '',
+
+  lowbattery: '',
+
+  emptybattery: '',
+
+  lightbulb: '💡',
+
+  downloadcloud: '',
+
+  download: '',
+
+  uploadcloud: '',
+
+  upload: '',
+
+  fork: '',
+
+  merge: '',
+
+  transfer: '⇆',
+
+  refresh: '↻',
+
+  sync: '',
+
+  loading: '',
+
+  wifi: '',
+
+  connection: '',
+
+  file: '📄',
+
+  folder: '📁',
+
+  quote: '“',
+
+  text: '',
+
+  font: '',
+
+  print: '⎙',
+
+  fax: '📠',
+
+  list: '',
+
+  layout: '',
+
+  action: '',
+
+  redirect: '↪',
+
+  expand: '⤢',
+
+  contract: '',
+
+  help: '❓',
+
+  info: 'ℹ',
+
+  alert: '⚠',
+
+  caution: '⛔',
+
+  logout: '',
+
+  plus: '+',
+
+  hyphen: '-',
+
+  check: '✓',
+
+  delete: '␡',
+
+  settings: '⚙',
+
+  dashboard: '',
+
+  notifications: '🔔',
+
+  notificationsdisabled: '🔕',
+
+  clock: '⏲',
+
+  stopwatch: '⏱',
+
+  calendar: '📅',
+
+  addcalendar: '',
+
+  removecalendar: '',
+
+  checkcalendar: '',
+
+  deletecalendar: '',
+
+  plane: '✈',
+
+  briefcase: '💼',
+
+  cloud: '☁',
+
+  droplet: '💧',
+
+  flask: '',
+
+  up: '⬆',
+
+  upright: '⬈',
+
+  right: '➡',
+
+  downright: '⬊',
+
+  down: '⬇',
+
+  downleft: '⬋',
+
+  left: '⬅',
+
+  upleft: '⬉',
+
+  navigateup: '',
+
+  navigateright: '▻',
+
+  navigatedown: '',
+
+  navigateleft: '◅',
+
+  directup: '▴',
+
+  directright: '▹',
+
+  dropdown: '▾',
+
+  directleft: '◃',
+
+  retweet: '',
+
+  /* Legacy classes */
+  volumelow: '🔉',
+
+  volumehigh: '🔊',
+
+  batteryhigh: '',
+
+  batterymedium: '',
+
+  batterylow: '',
+
+  batteryempty: '',
+
+  clouddownload: '',
+
+  cloudupload: '',
+
+  calendaradd: '',
+
+  calendarremove: '',
+
+  calendarcheck: '',
+
+  calendardelete: ''
+};
--- src/Navbar/NavItem.js
+++ src/Navbar/NavItem.js
@@ -1,12 +1,29 @@
 import React, { PropTypes } from 'react';
-import { nav, rightPosition } from './style.scss';
+import styled from 'styled-components';
 
-const NavItem = ({ children, className, right }) => (<ul className={`${nav} ${right && rightPosition} ${className}`}>{children}</ul>);
+const List = styled.ul`
+  margin: 0;
+  padding: 0;
+  float: ${props => (props.right ? 'right' : 'left')};
+
+  & > li {
+    display: inline-block;
+    margin: 15px 10px;
+
+    & > button {
+      margin-top: -10px;
+    }
+  }
+`;
+
+const NavItem = ({ children, className, right }) => (
+  <List right={right} className={className}>{children}</List>
+);
 
 NavItem.propTypes = {
   children: PropTypes.arrayOf(PropTypes.element),
   className: PropTypes.string,
-  right: PropTypes.boolean,
+  right: PropTypes.boolean
 };
 
 NavItem.defaultProps = {
@@ -14,6 +31,5 @@ NavItem.defaultProps = {
   className: '',
   right: false
 };
-
 
 export default NavItem;
